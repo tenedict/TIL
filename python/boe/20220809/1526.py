bebf8a49867a443e88d34b1c27485ae82ca1b44b
tenedict <tenedict@gmail.com>
2022-08-09 21:08:57 +0900

    0809

--- python/boe/20220809/1526.py
+++ python/boe/20220809/1526.py
@@ -1,19 +1,19 @@
 n = input()
-c = True
-for _ in range(100):
-    n = str(n)
-    g = 0
+co = True
+    
+while co:   
+    pp = 0 
+    len_ = len(n)
     for i in n:
-        len_ = len(str(n))
-        print(n)
         if i == '7' or i == '4':
-            g += 1
+            pp += 1
         else:
             n = int(n)
-            print(n)
             n -= 1
-            g = 0
-        if g == len_:
-            print(n)
-        
-            
+            n = str(n)
+            break
+    if pp == len_:
+        co = False
+        print(n)
+
+    
