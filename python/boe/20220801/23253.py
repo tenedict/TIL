f0919f8a7e1e6ff85055550ca93c78444e5d6805
tenedict <tenedict@gmail.com>
2022-08-13 22:26:59 +0900

    0813

--- python/boe/20220801/23253.py
+++ python/boe/20220801/23253.py
@@ -9,9 +9,10 @@ for j in range(1,b+1):
     bookti = list(map(int,input().split()))
     for k in range(books-1):
         if bookti[k] < bookti[k+1]:
-            print('No')
             p = False
         else: 
             continue
 if p:
     print('Yes')
+else:
+    print('No')
